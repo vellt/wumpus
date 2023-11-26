ba930aa5c2995cc4b505765abc70e82d06ceee3b
Szanto Benjamin <szantobenjamin97@gmail.com>
2023-11-26 13:34:16 +0100

    CI-CD piplene test

--- src/main/java/hu/nye/progtech/models/ConsoleColor.java
+++ src/main/java/hu/nye/progtech/models/ConsoleColor.java
@@ -8,7 +8,7 @@ public class ConsoleColor {
     public static final String RESET = "\033[0m";
     public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
 
-    public static final String GREEN = "\033[0;32m";   // GREEN
+    public static final String GREEN = "\033[0;32m";
 
-    public static final String BLUE = "\033[0;34m";    // BLUE
+    public static final String BLUE = "\033[0;34m";
 }
